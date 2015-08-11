224584c2c3d29ca2ea4564f7db02ccf5d51c6ee9
Bogdan Agafonov <bogdan.agafonov@gmail.com>
2015-08-11 19:37:18 +0300

    fixed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::API
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, X-Requested-With, X-Prototype-Version'
       headers['Access-Control-Max-Age'] = '1728000'
+      render text: "", status: 200
     end
   end
 
